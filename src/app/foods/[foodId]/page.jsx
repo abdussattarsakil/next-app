84a1a7fa3d708a062a6e95059cb0142a75c8ffc4
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-07-10 01:36:04 +0600

    style: foods details card

--- src/app/foods/[foodId]/page.jsx
+++ src/app/foods/[foodId]/page.jsx
@@ -6,22 +6,31 @@ const page = async ({ params }) => {
 
     const res = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/foods/${foodId}`);
     const data = await res.json();
-    const { dish_name,image_link } = data.data;
+    const { dish_name, image_link, main_ingredients } = data.data;
 
     return (
         <div className="container mx-auto">
-            <div className="card bg-base-100 image-full w-fit h-fit shadow-sm mx-auto">
+            <div className="card bg-base-200 image-full w-fit h-fit shadow-sm mx-auto">
                 <figure>
                     <Image
-                    src={image_link}
-                    alt={dish_name}
-                    width={300}
-                    height={100}
+                        src={image_link}
+                        alt={dish_name}
+                        width={350}
+                        height={200}
                     />
                 </figure>
                 <div className="card-body">
-                    <h2 className="card-title">Card Title</h2>
-                    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <h2 className="card-title">{dish_name}</h2>
+
+                    <ul className="list-disc mt-6">
+                        <h1>Ingredients:</h1>
+                        {main_ingredients.map((ingredient, index) =>
+                            <li key={index}>
+                                {ingredient}
+                            </li>)}
+                    </ul>
+
+
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">Buy Now</button>
                     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Navbar = () => {
     return (
         <div>
-            <div className="max-lg:collapse bg-base-200 lg:mb-48 shadow-sm w-full rounded-md">
+            <div className="max-lg:collapse bg-base-200 lg:mb-10 shadow-sm w-full rounded-md">
                 <input id="navbar-1-toggle" className="peer hidden" type="checkbox" />
                 <label htmlFor="navbar-1-toggle" className="fixed inset-0 hidden max-lg:peer-checked:block"></label>
                 <div className="collapse-title navbar">
