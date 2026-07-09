8eec1c9611394be6663a724a43a5c1855b14d2f7
abdussattarsakil <abdussattarsakil208@gmail.com>
2026-07-09 19:24:07 +0600

    btn add

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 
 const FoodCard = ({ food }) => {
-    const { dish_name, image_link } = food;
+    const { dish_name, image_link, price, rating, category } = food;
     return (
         <div>
             <div className="card bg-base-100 shadow-sm h-full p-5 ">
@@ -17,12 +17,12 @@ const FoodCard = ({ food }) => {
                 <div className="card-body">
                     <h2 className="card-title">
                         {dish_name}
-                        <div className="badge badge-secondary">NEW</div>
+                        <div className="badge badge-secondary">Rating:{rating}</div>
                     </h2>
                     <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
                     <div className="card-actions justify-end">
-                        <div className="badge badge-outline">Fashion</div>
-                        <div className="badge badge-outline">Products</div>
+                        <button className="btn btn-success">Add to cart</button>
+                        <button className="btn btn-info">Show details</button>
                     </div>
                 </div>
             </div>
